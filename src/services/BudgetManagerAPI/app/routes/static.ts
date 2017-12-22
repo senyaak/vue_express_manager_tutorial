d2dd9f5ff5f35981484c8db31571ff77649fb972
Arsenij Schützer <aschuetzer@saltation.de>
2017-12-22 11:39:55 +0100

    Check if the frontend path is set

--- src/services/BudgetManagerAPI/app/routes/static.ts
+++ src/services/BudgetManagerAPI/app/routes/static.ts
@@ -7,6 +7,9 @@ import {api} from "./../api/auth";
 import {models} from "./../setup/index";
 
 const config: {frontend: string} = require("../../../../../config/config.json");
+if(!config.frontend) {
+  throw new Error("no frontend path defined in config.json");
+}
 const frontend = `${__dirname}/../../../../../${config.frontend}/`;
 
 export default function(app: express.Application) {
